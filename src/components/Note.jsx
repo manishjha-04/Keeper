12d94c81039934153afa5d5757692e84c1c6d427
manishjha-04 <stayconnectedwithmanish@gmail.com>
2023-07-10 21:01:58 +0530

    created header section for keeper

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return (<div className="note">
+        <h1>This is for title</h1>
+        <p>this is for content</p>
+    </div>);
+}
+
+export default Note;
