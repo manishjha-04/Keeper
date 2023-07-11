da7e7267eae52a0fe542656a0d566a4b151f1f8d
manishjha-04 <stayconnectedwithmanish@gmail.com>
2023-07-12 00:25:02 +0530

    added material ui and add delete functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-// remember when you are using javascript (react is also javascript only) then dont forget to use camelcasing 
-
-
-function Footer(){
-    
-    const currentYear = new Date().getFullYear();
-        return (<footer>    
-        <p>copyright {currentYear}</p>
-    </footer>);
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
